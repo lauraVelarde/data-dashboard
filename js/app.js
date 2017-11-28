0a241f52b167afd9a2c31b5d46c6677dbf4a0eef
lauraVelarde <penelope_rebeca_19@hotmail.com>
2017-11-28 18:31:44 -0500

    añadiendo primera parte

--- js/app.js
+++ js/app.js
@@ -3,4 +3,9 @@
  */
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
+window.addEventListener('load', function(event) {
+  var AQP = data.AQP;
+  console.log(AQP['2016-2']);
+});
 console.log(data);
+
